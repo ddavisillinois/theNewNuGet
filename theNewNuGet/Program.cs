3f410b5f126e128518c9885164db379b27d4f122
ddavisillinois <ddavis.illinois@gmail.com>
2025-02-19 10:26:52 -0600

    Nuget discovery testing

--- theNewNuGet/Program.cs
+++ theNewNuGet/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// using ddPotLuckClassLibrary;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// var potLuck = new dtblRequestData();
+
+// var ddTemp4454 = new ddParticipantDTO();
